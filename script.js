c8a0ce3d58ec87daaa448351e8559af3e8b1c14a
Tristan Wong <tristanwong321@hotmail.com>
2022-05-08 16:57:57 -0400

    Finished grid functions/buttons and mouse events

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const container = document.querySelector('#container');
+const btn = document.querySelector('button.btn');
 
-for (let i=0; i<16; i++){
-    for (let j=0; j<16; j++){
-        let div = document.createElement('div');
-        div.textContent = i + "-" + j;
-        container.appendChild(div);
+function makeGrid(dimension){
+    while(container.firstChild)
+        container.removeChild(container.firstChild);    
+    
+    for (let i=0; i<dimension; i++){
+        for (let j=0; j<dimension; j++){
+            let gridItem = document.createElement('div');
+            gridItem.classList = 'grid-item';
+            gridItem.style.border = "thin solid black";
+            gridItem.style.textAlign = "center";
+            gridItem.addEventListener("mouseover", () => {
+                gridItem.style.backgroundColor = "green";
+            });
+            gridItem.addEventListener("mouseout", () => {
+                gridItem.style.transitionDelay = "1s";
+                gridItem.style.backgroundColor = "white";
+            });
+            container.appendChild(gridItem);
+            container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+            container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+        }
     }
 }
 
+makeGrid(16);
+
+btn.addEventListener('click', () => {
+    let dimension = prompt("Enter new grid dimension: ");
+    makeGrid(dimension);
+})
